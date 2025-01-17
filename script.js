5fe74291ea332dd6587cc8a88464f19b47cb6b3d
Vrushabh D B <51850418+vrushabd@users.noreply.github.com>
2025-01-17 22:36:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let btn = document.querySelector("button");
+let forgot = document.querySelector(".forgot");
+forgot.addEventListener("click",()=>{
+ alert("Not Functional By Vrushabh")
+});
 
 btn.addEventListener("click",()=>{
 alert("Button is Not Functionally Active")
